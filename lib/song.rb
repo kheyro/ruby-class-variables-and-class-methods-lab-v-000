4a6c86875e4ffcc376cc8f8b5e5f251684dcd259
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-10 18:21:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,30 @@
 class Song
-  attr_accessor: name, artist, genre
+  attr_accessor :name, :artist, :genre
 
   @@count = 0
   @@artists = []
   @@genres = []
 
+  def initialize(name, artist, genre)
+    self.name = name
+    self.artist = artist
+    self.genre = genre
+
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def count
+    puts @@count
+  end
+
+  def artists
+    @@artists
+  end
+
+  def genres
+    @@genres
+  end
 
 end
