8495d9e7c13f33f92b580bb3bffe676cb438eab4
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-10 19:04:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,9 +27,15 @@ class Song
     @@genres.uniq
   end
 
-  def self.genre_count
+  def self.artist_count
     test = Hash.new(0)
     @@artists.each { |artist| test[artist] += 1 }
+    test
+  end
+
+  def self.genre_count
+    test = Hash.new(0)
+    @@genres.each { |genre| test[genre] += 1 }
     test
   end
 
